c49fa94b027a7f53c5bf5f480965bc783b34c8a9
Jean Jar Pereira de Araújo <jeanjpa@gmail.com>
2020-06-10 15:59:44 -0300

    phpdoc

--- src/Eloquent/Concerns/UseUUIDAsPrimaryKey.php
+++ src/Eloquent/Concerns/UseUUIDAsPrimaryKey.php
@@ -4,8 +4,15 @@ namespace LaraStuffs\PrimaryUUID\Eloquent\Concerns;
 
 use Illuminate\Support\Str;
 
+/**
+ * Trait UseUUIDAsPrimaryKey
+ * @package LaraStuffs\PrimaryUUID\Eloquent\Concerns
+ */
 trait UseUUIDAsPrimaryKey
 {
+    /**
+     * Boot this traits on the model
+     */
     protected static function bootUseUUIDAsPrimaryKey()
     {
         self::creating(
@@ -15,11 +22,21 @@ trait UseUUIDAsPrimaryKey
         );
     }
 
+    /**
+     * Get the value indicating whether the IDs are incrementing.
+     *
+     * @return bool
+     */
     public function getIncrementing()
     {
         return false;
     }
 
+    /**
+     * Get the key type.
+     *
+     * @return string
+     */
     public function getKeyType()
     {
         return 'string';
